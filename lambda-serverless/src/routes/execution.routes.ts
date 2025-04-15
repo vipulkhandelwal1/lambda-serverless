fcf6afb5eea001868c04792ccc47ec2b9610faec
Vrashank Shetty <vrashankshetty.s@gmail.com>
2025-04-15 09:28:27 +0530

    changes in routes

---
+++ lambda-serverless/src/routes/execution.routes.ts
@@ -0,0 +1,14 @@
+
+// src/api/routes/execution.routes.ts
+import { Router } from 'express';
+import executionController from '../controllers/execution.controller';
+
+const router = Router();
+
+router.post('/run/:route', executionController.executeFunction);
+router.delete('/all', executionController.deleteAllExecution);
+router.get('/', executionController.getAllExecution);
+router.get('/:id', executionController.getEachExecution);
+router.post('/admin/cleanup', executionController.cleanupContainers);
+
+export default router;
